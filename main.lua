9bcc086dee215b38af3869da0a71d2edfb5f3c9f
Maida <maida.maxime@gmail.com>
2023-01-21 17:21:25 +0100

    Display hero + basic control

--- main.lua
+++ main.lua
@@ -1,13 +1,34 @@
+hero = {}
+
+hero.x = 50
+hero.y = 10
+
+local width
+local height
+
 function love.load()
     width = love.graphics.getWidth()
     height = love.graphics.getHeight()
 
-    hero = love.graphics.newImage("img/heros.png")
+    hero.img = love.graphics.newImage("img/heros.png")
 end
 
 function love.update(dt)
+    if love.keyboard.isDown("up") then
+        hero.y = hero.y - 1
+    end
+    if love.keyboard.isDown("right") then
+        hero.x = hero.x + 1
+    end
+
+    if love.keyboard.isDown("down") then
+        hero.y = hero.y + 1
+    end
+    if love.keyboard.isDown("left") then
+        hero.x = hero.x - 1
+    end
 end
 
 function love.draw()
-    love.graphics.draw(hero, 0, 0)
+    love.graphics.draw(hero.img, hero.x, hero.y)
 end
