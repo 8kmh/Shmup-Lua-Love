8e8a53387e6ec3a092fd1e282c70eacf89ed61a2
Maida <maida.maxime@gmail.com>
2023-01-23 00:20:55 +0100

    Shoot + Sound OK

--- main.lua
+++ main.lua
@@ -3,14 +3,18 @@ love.graphics.setDefaultFilter("nearest")
 
 heros = {}
 sprites = {}
+shoots = {}
 
 heros.x = 0
 heros.y = 0
+
+soundShoot = love.audio.newSource("sound/shoot.wav", "static")
 
 function createSprite(pImgName, pX, pY)
     sprite = {}
     sprite.x = pX
     sprite.y = pY
+    sprite.delete = false
     sprite.img = love.graphics.newImage("img/" .. pImgName .. ".png")
     sprite.width = sprite.img:getWidth()
     sprite.height = sprite.img:getHeight()
@@ -32,17 +36,35 @@ function love.load()
 end
 
 function love.update(dt)
-    if love.keyboard.isDown("up") then
+    local n
+    for n = #shoots, 1, -1 do
+        local shoot = shoots[n]
+        shoot.y = shoot.y + shoot.speed
+
+        -- check if the shoot is in the screen
+        if shoot.y < 0 or shoot.y > height then
+            shoot.delete = true
+            table.remove(shoots, n)
+        end
+    end
+
+    for n = #sprites, 1, -1 do
+        if sprites[n].delete == true then
+            table.remove(sprites, n)
+        end
+    end
+
+    if love.keyboard.isDown("up") and heros.y > 0 then
         heros.y = heros.y - 1
     end
-    if love.keyboard.isDown("right") then
+    if love.keyboard.isDown("right") and heros.x < width then
         heros.x = heros.x + 1
     end
 
-    if love.keyboard.isDown("down") then
+    if love.keyboard.isDown("down") and heros.y < height then
         heros.y = heros.y + 1
     end
-    if love.keyboard.isDown("left") then
+    if love.keyboard.isDown("left") and heros.x > 0 then
         heros.x = heros.x - 1
     end
 end
@@ -53,7 +75,16 @@ function love.draw()
         local s = sprites[n]
         love.graphics.draw(s.img, s.x, s.y, 0, 2, 2, s.width / 2, s.height / 2)
     end
+
+    love.graphics.print("shoot " .. #shoots, 0, 0)
 end
 
 function love.keypressed(key)
+    if key == "space" then
+        local shoot = createSprite("laser1", heros.x, heros.y - (heros.height * 2) / 2)
+        shoot.speed = -10
+        table.insert(shoots, shoot)
+
+        soundShoot:play()
+    end
 end
