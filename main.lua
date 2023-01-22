a839dc3bdc1bc7ac75c7811db99a59cbde384e74
Maida <maida.maxime@gmail.com>
2023-01-22 20:26:57 +0100

    Function createSprite

--- main.lua
+++ main.lua
@@ -1,34 +1,59 @@
-hero = {}
+-- Prevent love to filter images on resized
+love.graphics.setDefaultFilter("nearest")
 
-hero.x = 50
-hero.y = 10
+heros = {}
+sprites = {}
 
-local width
-local height
+heros.x = 0
+heros.y = 0
+
+function createSprite(pImgName, pX, pY)
+    sprite = {}
+    sprite.x = pX
+    sprite.y = pY
+    sprite.img = love.graphics.newImage("img/" .. pImgName .. ".png")
+    sprite.width = sprite.img:getWidth()
+    sprite.height = sprite.img:getHeight()
+
+    table.insert(sprites, sprite)
+
+    return sprite
+end
 
 function love.load()
+    love.window.setMode(1024, 768)
+    love.window.setTitle("Shmup")
+
     width = love.graphics.getWidth()
     height = love.graphics.getHeight()
 
-    hero.img = love.graphics.newImage("img/heros.png")
+    heros = createSprite("heros", width / 2, height / 2)
+    heros.y = height - (heros.height * 2)
 end
 
 function love.update(dt)
     if love.keyboard.isDown("up") then
-        hero.y = hero.y - 1
+        heros.y = heros.y - 1
     end
     if love.keyboard.isDown("right") then
-        hero.x = hero.x + 1
+        heros.x = heros.x + 1
     end
 
     if love.keyboard.isDown("down") then
-        hero.y = hero.y + 1
+        heros.y = heros.y + 1
     end
     if love.keyboard.isDown("left") then
-        hero.x = hero.x - 1
+        heros.x = heros.x - 1
     end
 end
 
 function love.draw()
-    love.graphics.draw(hero.img, hero.x, hero.y)
+    local n
+    for n = 1, #sprites do
+        local s = sprites[n]
+        love.graphics.draw(s.img, s.x, s.y, 0, 2, 2, s.width / 2, s.height / 2)
+    end
+end
+
+function love.keypressed(key)
 end
